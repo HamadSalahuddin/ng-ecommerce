3d47e427f8c4f4d1abbea9c9117e9aa60fc3a5ee
Hamad Salahuddin <43289397+HamadSalahuddin@users.noreply.github.com>
2026-05-13 14:52:05 +0500

    Made the store's title navigational

--- src/app/layout/header/header.ts
+++ src/app/layout/header/header.ts
@@ -1,17 +1,19 @@
 import { Component } from '@angular/core';
 import { MatToolbar } from '@angular/material/toolbar';
-import { HeaderActions } from "../header-actions/header-actions";
+import { HeaderActions } from '../header-actions/header-actions';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [MatToolbar, HeaderActions],
+  imports: [MatToolbar, HeaderActions, RouterLink],
   template: `
-  <mat-toolbar class="w-full elevated py-2">
-    <div class="max-w-300 mx-auto w-full flex items-center justify-between">
-      <span>Modern Store</span>
-      <app-header-actions />
-    </div>
-  </mat-toolbar> `,
+    <mat-toolbar class="w-full elevated py-2">
+      <div class="max-w-300 mx-auto w-full flex items-center justify-between">
+        <span class="hover:cursor-pointer" routerLink="/">Modern Store</span>
+        <app-header-actions />
+      </div>
+    </mat-toolbar>
+  `,
   styles: ``,
 })
 export class Header {}
